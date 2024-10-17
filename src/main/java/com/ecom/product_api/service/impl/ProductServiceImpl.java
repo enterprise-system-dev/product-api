e5dde36c22e61d938b736bacdf27b255fd866191
ozen-hub <info.ozenhub@gmail.com>
2024-10-17 21:21:59 +0530

    entity created

--- src/main/java/com/ecom/product_api/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/product_api/service/impl/ProductServiceImpl.java
@@ -2,11 +2,13 @@ package com.ecom.product_api.service.impl;
 
 import com.ecom.product_api.dto.CommonFileSavedBinaryDataDto;
 import com.ecom.product_api.dto.request.RequestProductDto;
+import com.ecom.product_api.dto.response.ResponseImage;
 import com.ecom.product_api.dto.response.ResponseProductDto;
 import com.ecom.product_api.dto.response.paginate.ResponseProductPaginate;
 import com.ecom.product_api.entity.FileResource;
 import com.ecom.product_api.entity.Images;
 import com.ecom.product_api.entity.Product;
+import com.ecom.product_api.exception.EntryNotFoundException;
 import com.ecom.product_api.repository.ProductRepo;
 import com.ecom.product_api.service.FileService;
 import com.ecom.product_api.service.ProductService;
@@ -19,9 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @Transactional
@@ -36,17 +36,17 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void createProduct(RequestProductDto dto, MultipartFile file) {
-        CommonFileSavedBinaryDataDto resource =null;
-        try{
-            resource = fileService.create(file,"abc/images/srilanka/",bucketName);
+        CommonFileSavedBinaryDataDto resource = null;
+        try {
+            resource = fileService.create(file, "abc/images/srilanka/", bucketName);
             Set<Images> images = new HashSet<>();
             images.add(
                     Images.builder()
                             .fileResource(
-                                   new FileResource(
-                                           resource.getHash(), resource.getFileName(),
-                                           resource.getResourceUrl(), resource.getDirectory()
-                                   )
+                                    new FileResource(
+                                            resource.getHash(), resource.getFileName(),
+                                            resource.getResourceUrl(), resource.getDirectory()
+                                    )
                             )
                             .id(UUID.randomUUID().toString())
                             .build()
@@ -60,25 +60,60 @@ public class ProductServiceImpl implements ProductService {
                     .build();
             productRepo.save(product);
         } catch (SQLException | IOException e) {
-                fileService.delete(fileDataExtractor.blobToString(resource.getDirectory()),
-                        fileDataExtractor.blobToString(resource.getFileName()),bucketName);
+            fileService.delete(fileDataExtractor.blobToString(resource.getDirectory()),
+                    fileDataExtractor.blobToString(resource.getFileName()), bucketName);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void updateProduct(RequestProductDto dto, String productId) {
-
+        Optional<Product> selectedProductData = productRepo.findById(productId);
+        if (selectedProductData.isEmpty()) {
+            throw new EntryNotFoundException("Product not found");
+        }
+        selectedProductData.get().setDescription(dto.getDescription());
+        selectedProductData.get().setQty(dto.getQty());
+        selectedProductData.get().setUnitPrice(dto.getUnitPrice());
+        productRepo.save(selectedProductData.get());
     }
 
     @Override
     public void deleteProduct(String productId) {
-
+        Optional<Product> selectedProductData = productRepo.findById(productId);
+        if (selectedProductData.isEmpty()) {
+            throw new EntryNotFoundException("Product not found");
+        }
+        productRepo.deleteById(productId);
     }
 
     @Override
     public ResponseProductDto findProductById(String productId) {
-        return null;
+        Optional<Product> selectedProductData = productRepo.findById(productId);
+        if (selectedProductData.isEmpty()) {
+            throw new EntryNotFoundException("Product not found");
+        }
+        return convert(selectedProductData.get());
+    }
+
+    ResponseProductDto convert(Product p) {
+        if (p == null) {
+            return null;
+        }
+
+        List<ResponseImage> images = new ArrayList<>();
+        for (Images i : p.getImages()) {
+            images.add(
+                    new ResponseImage(i.getId(), fileDataExtractor.blobToString(i.getFileResource().getResourceUrl()))
+            );
+        }
+        return ResponseProductDto.builder()
+                .productId(p.getProductId())
+                .unitPrice(p.getUnitPrice())
+                .qty(p.getQty())
+                .description(p.getDescription())
+                .images(images)
+                .build();
     }
 
     @Override
