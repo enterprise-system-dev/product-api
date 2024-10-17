dfe87bf592f8cd6de75641cd260029f2e9fc30df
ozen-hub <info.ozenhub@gmail.com>
2024-10-17 20:44:22 +0530

    entity created

--- src/main/java/com/ecom/product_api/service/FileService.java
+++ src/main/java/com/ecom/product_api/service/FileService.java
@@ -3,7 +3,10 @@ package com.ecom.product_api.service;
 import com.ecom.product_api.dto.CommonFileSavedBinaryDataDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface FileService {
-    public CommonFileSavedBinaryDataDto create(MultipartFile file, String directory, String bucket);
+    public CommonFileSavedBinaryDataDto create(MultipartFile file, String directory, String bucket) throws IOException, SQLException;
     public void delete(String directory, String fileName, String bucket);
 }
