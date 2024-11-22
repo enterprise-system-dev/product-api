14c0ceeb7dac48efc426fc99f0a19dbfa6a5b873
education-bootcamp <education.seekerscloud@gmail.com>
2024-11-22 10:36:20 +0530

    entity created

--- src/main/java/com/ecom/product_api/api/ProductController.java
+++ src/main/java/com/ecom/product_api/api/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/products-service/api/v1/products")
+@RequestMapping("/product-service/api/v1/products")
 @RequiredArgsConstructor
 // loggers
 public class ProductController {
